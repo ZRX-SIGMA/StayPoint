93191646168f246ece8c6716f78e8e9dfeccd98d
ZRX-SIGMA <sk.jiyad2005@gmail.com>
2025-06-28 22:16:05 +0530

    Initial Commit

---
+++ StayPoint/src/test/java/com/jiyad/StayPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiyad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
